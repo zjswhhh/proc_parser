ea3955632bb1d4d4489b2bc3b19e53eb78138565
Jing Zhang <jzh136@ms07.csug.rochester.edu>
2018-09-14 21:03:41 -0400

    get total mem done

--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <vector>
 using namespace std;
 
 float get_cpu_clock_speed () {
     FILE* fp;
-    char buffer[1024];
+    char buffer[1024*10];
     size_t bytes_read;
     char* match;
     float clock_speed;
@@ -59,10 +58,29 @@ float get_amount_time_since_booted () {
     return amount_time;
 }
 
+float get_total_mem () {
+    FILE* fp;
+    float total_mem;
+    char buffer[2048];
+    size_t bytes_read;
+    
+    fp = fopen("/proc/meminfo", "r");
+    bytes_read = fread(buffer, 1, sizeof(buffer), fp);
+    fclose(fp);
+
+    if (bytes_read == 0 || bytes_read == sizeof(buffer))
+        return 0;
+    buffer[bytes_read] = '\0';
+    sscanf(buffer, "MemTotal:        %f", &total_mem);
+    return total_mem;
+}
+
+
 int main (){
     printf("%f\n", get_cpu_clock_speed());
     printf("%f\n", get_amount_time_since_booted());
     char s[30];
     printf("%s\n", get_kernel_version(s, 30));
+    printf("%f KB\n", get_total_mem());
     return 0;
 }
