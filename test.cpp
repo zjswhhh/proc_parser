2d43f0561a3cf16b7727c0f42ab27e6639e940ed
zjswhhh <zjsw1206@gmail.com>
2018-09-15 17:31:15 -0400

    test

--- test.cpp
+++ test.cpp
@@ -4,6 +4,16 @@
 #include <string>
 using namespace std;
 
+char* get_kernel_version (char* s) {
+    FILE* fp;
+    
+    fp = fopen("/proc/version", "r");
+    fscanf(fp, "Linux version %s (", s);
+    fclose(fp);
+
+    return   s; 
+}
+
 char* get_processor_type (char* s) {
     FILE* fp;
     char buffer[1024];
@@ -22,16 +32,6 @@ char* get_processor_type (char* s) {
         return s;
     sscanf(match, "model name      : %[^\t\n] stepping ", s);
     return s;
-}
-
-char* get_kernel_version (char* s) {
-    FILE* fp;
-    
-    fp = fopen("/proc/version", "r");
-    fscanf(fp, "Linux version %s (", s);
-    fclose(fp);
-
-    return   s; 
 }
 
 double get_amount_time_since_booted () {
@@ -69,7 +69,18 @@ void get_time_percentage (float result[3]) {
     result[0] = ((float)(user_time/total));
     result[1] = ((float)(sys_time/total));
     result[2] = ((float)(idle/total));
+}
 
+void get_free_mem_amount_and_percentage (float result[2]) {
+    FILE* fp;
+    float mem_total, mem_free;
+    
+    fp = fopen("/proc/meminfo", "r");
+    fscanf(fp, "MemTotal:       %f kB \nMemFree:        %f kB", &mem_total, &mem_free);
+    fclose(fp);
+
+    result[0] = mem_free;
+    result[1] = mem_free/mem_total;
 }
 
 int main (int argc, char** argv){
@@ -79,13 +90,19 @@ int main (int argc, char** argv){
         printf("Kernel Version: %s \n", get_kernel_version(s));
         printf("The amount of memory configured into this computer: %.1f KB \n", get_total_mem());
 	printf("Amount of time since the system was last booted: %.1lf Seconds \n", get_amount_time_since_booted());
-    }  
+    }
     else if(argc == 3) {
         float t[3];
         get_time_percentage(t);
+        printf("The percentage of time the processor spend in User Mode, System Mode, and Idle will be: \n");
         for(int i = 0; i < 3; i++)
-            printf("%f\% ", t[i]*100.0);
+            printf("%f%%\t", t[i]*100.0);
         printf("\n");
+
+        float m[2];
+        get_free_mem_amount_and_percentage(m);
+
+        printf("%f KB %f\n", m[0], m[1]*100.0);
     }
     else {
     	printf("Tip: \nPlease run without parameter to call Version 1, or run with <read_rate> <print_out> to call Version 2.\n");
@@ -93,3 +110,5 @@ int main (int argc, char** argv){
     
     return 0;
 }
+
+
